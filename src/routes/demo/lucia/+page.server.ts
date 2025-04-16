e22f60f157bf7ca1691360ce50a415f2a1300565
Devarsh <75148774+webdev03@users.noreply.github.com>
2025-04-16 18:14:03 +1200

    formatting

--- src/routes/demo/lucia/+page.server.ts
+++ src/routes/demo/lucia/+page.server.ts
@@ -3,20 +3,20 @@ import { fail, redirect } from '@sveltejs/kit';
 import type { Actions, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
-	if (!event.locals.user) {
-		return redirect(302, '/demo/lucia/login');
-	}
-	return { user: event.locals.user };
+  if (!event.locals.user) {
+    return redirect(302, '/demo/lucia/login');
+  }
+  return { user: event.locals.user };
 };
 
 export const actions: Actions = {
-	logout: async (event) => {
-		if (!event.locals.session) {
-			return fail(401);
-		}
-		await auth.invalidateSession(event.locals.session.id);
-		auth.deleteSessionTokenCookie(event);
+  logout: async (event) => {
+    if (!event.locals.session) {
+      return fail(401);
+    }
+    await auth.invalidateSession(event.locals.session.id);
+    auth.deleteSessionTokenCookie(event);
 
-		return redirect(302, '/demo/lucia/login');
-	}
+    return redirect(302, '/demo/lucia/login');
+  },
 };
