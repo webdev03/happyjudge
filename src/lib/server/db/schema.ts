cc2ae954fd8e0acb7b1023a3d9abcd5dec7601fe
Devarsh <75148774+webdev03@users.noreply.github.com>
2025-04-17 21:23:09 +1200

    schema updates

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import { pgTable, pgEnum, text, timestamp, integer, serial, jsonb, boolean } from 'drizzle-orm/pg-core';
 
 export const user = pgTable('user', {
   id: text('id').primaryKey(),
@@ -17,6 +17,60 @@ export const session = pgTable('session', {
   }).notNull(),
 });
 
+export const difficultyEnum = pgEnum('difficulty', ['easy', 'medium', 'hard', 'expert', 'insane']);
+
+export const problem = pgTable('problem', {
+  id: text('id').primaryKey(),
+  title: text('title').notNull(),
+  statement: text('statement').notNull(),
+  difficulty: difficultyEnum('difficulty').notNull(),
+  timeLimit: integer('time_limit').notNull(), // in milliseconds
+  memoryLimit: integer('memory_limit').notNull(), // in kilobytes
+  sampleTestcases: jsonb('sample_testcases').$type<{ input: string; output: string }[]>(),
+  authorId: text('author_id')
+    .notNull()
+    .references(() => user.id),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  // TODO: One to many access (first TODO groups)
+  tags: jsonb('tags').$type<string[]>().notNull(),
+});
+
+export const testcase = pgTable('testcase', {
+  id: serial('id').primaryKey(),
+  problemId: integer('problem_id')
+    .notNull()
+    .references(() => problem.id),
+  input: text('input').notNull(),
+  output: text('output').notNull(),
+  isHidden: boolean('is_hidden').default(true).notNull(),
+  weight: integer('weight').default(1).notNull(), // partial scoring
+});
+
+export const verdictEnum = pgEnum('verdict', [
+  'accepted',
+  'wrong_answer',
+  'time_limit_exceeded',
+  'memory_limit_exceeded',
+  'runtime_error',
+  'compilation_error',
+  'waiting',
+  'processing',
+]);
+
+// TODO: Submission
+
+// TODO: Group
+
+// TODO: Contest
+
 export type Session = typeof session.$inferSelect;
 
 export type User = typeof user.$inferSelect;
+
+export type DifficultyEnum = typeof difficultyEnum.enumValues;
+
+export type Problem = typeof problem.$inferSelect;
+
+export type Testcase = typeof testcase.$inferSelect;
+
+export type VerdictEnum = typeof verdictEnum.enumValues;
